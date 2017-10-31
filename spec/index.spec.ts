2fa35f24326abd4a1c0eb09fd2d1e7a73af6de7f
Anthropic <iamanthropic@gmail.com>
2017-10-31 22:27:13 +1100

    Update test to loop validators

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,13 +1,14 @@
 import { expect, should } from 'chai';
-import * as djv from 'djv';
 import * as Assimilate from '../dist/package/Assimilate';
-import '../dist/package/use/validator/djv';
 import {
   ValidatorLibraryInstance,
   AssimilateAPI,
   DefaultOptions,
   RequiredOptions,
 } from '../dist/package/declarations';
+console.log(Assimilate.Validator.libraries);
+console.log(Object.keys(Assimilate.Validator.libraries));
+const libraries: string[] = Object.keys(Assimilate.Validator.libraries);
 
 should();
 
@@ -35,7 +36,6 @@ describe('Assimilate.Validator', () => {
 
     describe('addLibrary', () => {
       it('should addLibrary', () => {
-        // console.log('Assimilate.Validator.libraries', Assimilate.Validator.libraries['djv'].lib.toString());
         Assimilate.Validator.libraries.should.be.a('object');
         Assimilate.Validator.libraries.should.not.haveOwnProperty('test');
         Assimilate.Validator.addLibrary('test', TestTrue);
@@ -109,84 +109,112 @@ describe('Assimilate.Validator', () => {
           ]
         };
 
-        describe('djv', () => {
-          it('validates against djv', () => {
-            Assimilate.Validator.use('djv');
-            Assimilate.Validator.addSchema('arrayTestdjv', arrayTest);
-            Assimilate.Validator.validate('arrayTestdjv', { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
-            console.log("arrayTest djv", Assimilate.Validator.getErrors());
-
-            Assimilate.Validator.addSchema('enumTestdjv', enumTest);
-            Assimilate.Validator.validate('enumTestdjv', { "test": 'A' });
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("enumTest djv pass", Assimilate.Validator.getErrors());
-            Assimilate.Validator.validate('enumTestdjv', { "test": 'B' });
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("enumTest djv fail", Assimilate.Validator.getErrors());
-
-            Assimilate.Validator.addSchema('ofTest', ofTest);
-            Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("ofTest djv pass", Assimilate.Validator.getErrors());
-            Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("ofTest djv fail", Assimilate.Validator.getErrors());
+        for(let i in libraries) {
+          let lib = libraries[i];
+          describe(lib, () => {
+            it('validates against ' + lib, () => {
+              Assimilate.Validator.use(lib);
+              Assimilate.Validator.addSchema('arrayTest' + lib, arrayTest);
+              Assimilate.Validator.validate('arrayTest' + lib, { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
+              console.log('arrayTest ' + lib, Assimilate.Validator.getErrors());
+  
+              Assimilate.Validator.addSchema('enumTest' + lib, enumTest);
+              Assimilate.Validator.validate('enumTest' + lib, { "test": 'A' });
+              Assimilate.Validator.getErrors().should.be.an('Array');
+              console.log('enumTest ' + lib + ' pass', Assimilate.Validator.getErrors());
+              Assimilate.Validator.validate('enumTest' + lib, { "test": 'B' });
+              Assimilate.Validator.getErrors().should.be.an('Array');
+              console.log('enumTest ' + lib + ' fail', Assimilate.Validator.getErrors());
+  
+              Assimilate.Validator.addSchema('ofTest', ofTest);
+              Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
+              Assimilate.Validator.getErrors().should.be.an('Array');
+              console.log('ofTest ' + lib + ' pass', Assimilate.Validator.getErrors());
+              Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
+              Assimilate.Validator.getErrors().should.be.an('Array');
+              console.log('ofTest ' + lib + ' fail', Assimilate.Validator.getErrors());
+            });
           });
-        });
+        }
 
-        describe('ajv', () => {
-          it('validates against ajv', () => {
-            Assimilate.Validator.use('ajv');
-            Assimilate.Validator.addSchema('arrayTestajv', arrayTest);
-            Assimilate.Validator.validate('arrayTestajv', { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
-            console.log("arrayTest ajv", Assimilate.Validator.getErrors());
+        // describe('djv', () => {
+        //   it('validates against djv', () => {
+        //     Assimilate.Validator.use('djv');
+        //     Assimilate.Validator.addSchema('arrayTestdjv', arrayTest);
+        //     Assimilate.Validator.validate('arrayTestdjv', { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
+        //     console.log("arrayTest djv", Assimilate.Validator.getErrors());
 
-            Assimilate.Validator.addSchema('enumTestajv', enumTest);
-            Assimilate.Validator.validate('enumTestajv', { "test": 'A' });
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("enumTest ajv pass", Assimilate.Validator.getErrors());
-            Assimilate.Validator.validate('enumTestajv', { "test": 'B' });
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            let enumTestAJVFail = Assimilate.Validator.getErrors();
-            enumTestAJVFail.length.should.eq(1);
-            console.log("enumTest ajv fail", enumTestAJVFail);
+        //     Assimilate.Validator.addSchema('enumTestdjv', enumTest);
+        //     Assimilate.Validator.validate('enumTestdjv', { "test": 'A' });
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("enumTest djv pass", Assimilate.Validator.getErrors());
+        //     Assimilate.Validator.validate('enumTestdjv', { "test": 'B' });
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("enumTest djv fail", Assimilate.Validator.getErrors());
+
+        //     Assimilate.Validator.addSchema('ofTest', ofTest);
+        //     Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("ofTest djv pass", Assimilate.Validator.getErrors());
+        //     Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("ofTest djv fail", Assimilate.Validator.getErrors());
+        //   });
+        // });
+
+        // describe('ajv', () => {
+        //   it('validates against ajv', () => {
+        //     Assimilate.Validator.use('ajv');
+        //     Assimilate.Validator.addSchema('arrayTestajv', arrayTest);
+        //     Assimilate.Validator.validate('arrayTestajv', { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
+        //     console.log("arrayTest ajv", Assimilate.Validator.getErrors());
+
+        //     Assimilate.Validator.addSchema('enumTestajv', enumTest);
+        //     Assimilate.Validator.validate('enumTestajv', { "test": 'A' });
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("enumTest ajv pass", Assimilate.Validator.getErrors());
+        //     Assimilate.Validator.validate('enumTestajv', { "test": 'B' });
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     let enumTestAJVFail = Assimilate.Validator.getErrors();
+        //     enumTestAJVFail.length.should.eq(1);
+        //     console.log("enumTest ajv fail", enumTestAJVFail);
             
-            Assimilate.Validator.addSchema('ofTest', ofTest);
-            Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("ofTest ajv pass", Assimilate.Validator.getErrors());
-            Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("ofTest ajv fail", Assimilate.Validator.getErrors());
-          });
-        });
+        //     Assimilate.Validator.addSchema('ofTest', ofTest);
+        //     Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("ofTest ajv pass", Assimilate.Validator.getErrors());
+        //     Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("ofTest ajv fail", Assimilate.Validator.getErrors());
+        //   });
+        // });
 
-        describe('tv4', () => {
-          it('validates against tv4', () => {
-            Assimilate.Validator.use('tv4');
-            Assimilate.Validator.addSchema('arrayTesttv4', arrayTest);
-            Assimilate.Validator.validate('arrayTesttv4', { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
-            console.log("arrayTest tv4", Assimilate.Validator.getErrors());
+        // describe('tv4', () => {
+        //   it('validates against tv4', () => {
+        //     Assimilate.Validator.use('tv4');
+        //     Assimilate.Validator.addSchema('arrayTesttv4', arrayTest);
+        //     Assimilate.Validator.validate('arrayTesttv4', { "test": [{ "alpha": "4" }, { "alpha": 4 }]});
+        //     console.log("arrayTest tv4", Assimilate.Validator.getErrors());
 
-            Assimilate.Validator.addSchema('enumTesttv4', enumTest);
-            Assimilate.Validator.validate('enumTesttv4', { "test": 'A' });
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("enumTest tv4 pass", Assimilate.Validator.getErrors());
-            Assimilate.Validator.validate('enumTesttv4', { "test": 'B' });
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            let enumTestTV4Fail = Assimilate.Validator.getErrors();
-            enumTestTV4Fail.length.should.eq(1);
-            console.log("enumTest tv4 fail", enumTestTV4Fail);
+        //     Assimilate.Validator.addSchema('enumTesttv4', enumTest);
+        //     Assimilate.Validator.validate('enumTesttv4', { "test": 'A' });
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("enumTest tv4 pass", Assimilate.Validator.getErrors());
+        //     Assimilate.Validator.validate('enumTesttv4', { "test": 'B' });
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     let enumTestTV4Fail = Assimilate.Validator.getErrors();
+        //     enumTestTV4Fail.length.should.eq(1);
+        //     console.log("enumTest tv4 fail", enumTestTV4Fail);
             
-            Assimilate.Validator.addSchema('ofTest', ofTest);
-            Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("ofTest tv4 pass", Assimilate.Validator.getErrors());
-            Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
-            Assimilate.Validator.getErrors().should.be.an('Array');
-            console.log("ofTest tv4 fail", Assimilate.Validator.getErrors());
-          });
-        });
+        //     Assimilate.Validator.addSchema('ofTest', ofTest);
+        //     Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }]});
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("ofTest tv4 pass", Assimilate.Validator.getErrors());
+        //     Assimilate.Validator.validate('ofTest', { "test": [{ "alpha": "a" }, { "gamma": "4" }]});
+        //     Assimilate.Validator.getErrors().should.be.an('Array');
+        //     console.log("ofTest tv4 fail", Assimilate.Validator.getErrors());
+        //   });
+        // });
       });
     });
 
